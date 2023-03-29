203149fe358b0f5d048fd24b612ec8389c647657
Joshua Jones <jmj52@iu.edu>
2023-03-28 22:57:38 -0400

    Pushing basic add, edit, and delete logic

--- mongodb_testconn.py
+++ mongodb_testconn.py
@@ -9,10 +9,12 @@ from pymongo import MongoClient
 import certifi
 from pprint import pprint
 import pandas as pd
+from bson.objectid import ObjectId
+
 
 
 # Returns all records for a given username
-def get_user_docs(a_collection,a_username):
+def get_user_docs(a_collection, a_username):
     query = a_collection.find({'User_Name':a_username})
     return pd.DataFrame.from_records(query)
 
@@ -36,15 +38,54 @@ def main():
     # Select collection
     mypct_cln = mypct_db['tracker']
 
-
+    #print(mypct_db.tracker.find_one())
+    
     ### Queries ###
 
     # Get all records from a specific user
-    username = 'test_user'
-    records = get_user_docs(mypct_cln, username)
-    print(records.head())
+    # username = 'test_user'
+    # records = get_user_docs(mypct_cln, username)
+    #print(records.head())
 
+    # Delete all 
+    # mypct_cln.delete_many({})
+    # exit()
 
+    # Edit Content
+    current_user_id = "64223e0a2529c24efa6f4701"
+    current_content_title = 'Inserted Title'
+    edited_content_title  = 'Revised Title'
+    query = {'_id':ObjectId(current_user_id),'Content.Title':current_content_title}
+    change = {'$set':{'Content.$.Title':edited_content_title}}
+    mypct_cln.update_one(filter=query,update=change)
+
+    query = {'_id':ObjectId(current_user_id)}
+    record = mypct_cln.find(query)
+    df = pd.DataFrame.from_records(record)
+    print(df)
+    exit()
+
+    # Add Content
+    current_user_id = "64223e0a2529c24efa6f4701"
+    current_content_title = 'Inserted Title'
+    query = {'_id':ObjectId(current_user_id)}
+    change = {'$push':{'Content':{'Title': current_content_title}}}
+    mypct_cln.update_one(filter=query,update=change)
+
+    record = mypct_cln.find(query)
+    df = pd.DataFrame.from_records(record)
+    print(df)
+
+    # Delete Content
+    current_user_id = "64223e0a2529c24efa6f4701"
+    current_content_title = 'This Is A Title'
+    query = {'_id':ObjectId(current_user_id)}
+    change = {'$pull':{'Content':{'Title': current_content_title}}}
+    mypct_cln.update_one(filter=query,update=change)
+    
+    record = mypct_cln.find(query)
+    df = pd.DataFrame.from_records(record)
+    print(df)
 
 if __name__ == '__main__':
     main()
