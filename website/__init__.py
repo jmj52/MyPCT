f8fc51ae428409cb0027b6c84c6bf31e329f8d20
m-lee1124 <128647222+m-lee1124@users.noreply.github.com>
2023-04-10 20:06:30 -0400

    loginandsignup

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import certifi
+
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdf'
+    app.config["MONGO_URI"] = "mongodb+srv://kenzie:kenziekenzie@cluster0.cgmpycw.mongodb.net/test"
+    mongo.init_app(app, tlsCAFile=certifi.where())
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, session, redirect, url_for
+from website import mongo
+from flask_pymongo import PyMongo
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        existing_user = mongo.db.tracker.find_one({'User_Email' : email, 'User_Password': password})
+        if existing_user is None:
+            print("No Account Exists")
+            return render_template('signup.html')
+        return redirect(url_for('views.home'))
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return '<p>logout</p>'
 
-@auth.route('/sign-up')
-def sign_up():
-    return '<p>Sign Up</p>'
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        existing_user = mongo.db.tracker.find_one({'User_email' : email})
+        
+        if existing_user is None:
+            mongo.db.tracker.insert_one({'First_Name': firstName, 'Last_Name': lastName, 'User_Email': email, 'User_Password': password})
+            session['User_Email'] = email
+            return redirect(url_for('views.home'))
+        
+        return 'That email already exists!'
+    
+    return render_template('signup.html')
