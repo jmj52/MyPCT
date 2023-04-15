d3888eff5d8053689cc0a05314d4752b79004c5d
m-lee1124 <128647222+m-lee1124@users.noreply.github.com>
2023-04-14 21:43:31 -0400

    loginandsignupdone

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from website import mongo
-from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth',__name__)
 
@@ -9,12 +9,19 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        existing_user = mongo.db.tracker.find_one({'User_Email' : email, 'User_Password': password})
-        if existing_user is None:
-            print("No Account Exists")
-            return render_template('signup.html')
-        return redirect(url_for('views.home'))
-    return render_template("login.html")
+        user = mongo.cx.mypct.tracker.find_one({'User_Email' : email})
+        if user:
+            # if check_password_hash(user['User_Password'], password):
+            if user['User_Password'] == password:
+                session['User_Email'] = email
+                return redirect(url_for('views.home'))
+            else:
+                print('Incorrect password, try again.')
+        else:
+            print('Email does not exist.')
+        
+    return render_template('login.html')
+
 
 @auth.route('/logout')
 def logout():
@@ -28,10 +35,11 @@ def signup():
         email = request.form.get('email')
         password = request.form.get('password')
         
-        existing_user = mongo.db.tracker.find_one({'User_email' : email})
+        existing_user = mongo.cx.mypct.tracker.find_one({'User_Email' : email})
         
         if existing_user is None:
-            mongo.db.tracker.insert_one({'First_Name': firstName, 'Last_Name': lastName, 'User_Email': email, 'User_Password': password})
+            #hashed = generate_password_hash(password, method='sha256')
+            mongo.cx.mypct.tracker.insert_one({'First_Name': firstName, 'Last_Name': lastName, 'User_Email': email, 'User_Password': password})
             session['User_Email'] = email
             return redirect(url_for('views.home'))
         
