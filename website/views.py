ecec480e05bb0bf9ebad45780f5624c114e98bb5
Joshua Jones <jmj52@iu.edu>
2023-04-15 11:17:17 -0400

    Adding edit content functionality

--- website/views.py
+++ website/views.py
@@ -15,6 +15,7 @@ views = Blueprint('views',__name__)
 def home():
     mypct_cln = mongo.cx.mypct.tracker
     contents = mypct_cln.find()
+    print(contents)
     return render_template('index.html',contents=contents)
 
 @views.route('/add_content', methods=['POST'])
@@ -23,15 +24,96 @@ def add_content():
 
     content_item = request.form.get('add-content')
     
-    query = get_user_docs(mypct_cln,'test_user')
-    print(query.head())
-    current_content_title = 'Title Updated via Flask-add'
-    query = {'User_Name':'test_user'}
-    change = {'$push':{'Content':{'Title': current_content_title}}}
-    mypct_cln.update_one(filter=query,update=change)
+    print(f'\nadd-content:{content_item}')
+    # query = get_user_docs(mypct_cln,'test_user')
+    # print(query.head())
+    # current_content_title = 'Title Updated via Flask-add'
+    # query = {'User_Name':'test_user'}
+    # change = {'$push':{'Content':{'Title': current_content_title}}}
+    # mypct_cln.update_one(filter=query,update=change)
 
     #mypct_cln.insert_one({'text': content_item})
-    print(content_item)
+    return redirect(url_for('views.home'))
+
+@views.route('/edit_content', methods=['GET', 'POST'])
+def edit_content():
+    print(f'\nedit_content:{request.method}\n')
+    mypct_cln = mongo.cx.mypct.tracker
+
+
+    # Onur Provide Index or Title of Content Item
+    selection_idx = 0
+    
+    # Kenzie to provide user_name or _id
+    current_user_id = "64223e0a2529c24efa6f4701"
+    query = {'_id':ObjectId(current_user_id)}
+    record = mypct_cln.find(query)
+    
+    # Get content from data retrieved
+    df = pd.DataFrame.from_records(record)
+    user_content_data = df['Content'][0]
+
+    # Use index of selected content to show content
+    user_content_data_sel = user_content_data[selection_idx]
+    #print(f'\nuser_data:\n{user_content_data_sel}')
+
+    current_content_title = user_content_data_sel['Title']
+
+    #Remove Artwork from results
+    remove_key = 'Artwork'
+    if remove_key in user_content_data_sel: user_content_data_sel.pop(remove_key)
+
+    # POST runs when user selects Save Changes
+    if request.method == 'POST':
+        content_item = request.form.getlist('edit-content')
+        print('content-passed:',content_item)
+
+        # Assume Content Is Not Valid until we can validate the user input
+        # Override to true for now, no validation criteria setup
+        content_valid = True
+
+        # Some Logic or Rules to validate various fields
+
+        # Content Is Not Valid, return to edit page with original content
+        if not(content_valid):
+            return render_template('edit_content.html',content_selected = user_content_data_sel)
+
+        # Content Is Valid, update database and return to content page
+        else:
+            
+            # Update Database
+            fields = list(user_content_data_sel.keys())
+            
+            query = {'_id':ObjectId(current_user_id),'Content.Title':current_content_title}
+            
+            change = {'$set':
+                      {'Content.$.Title':           content_item[0],
+                       'Content.$.Release_Date':    content_item[1],       
+                       'Content.$.Type':            content_item[2],
+                       'Content.$.Rating':          content_item[3],
+                       'Content.$.Genre':           content_item[4],
+                       'Content.$.Notes':           content_item[5],      
+                       'Content.$.Links':           content_item[6],       
+                        }}
+            
+            mypct_cln.update_one(filter=query,update=change)
+            
+            return redirect(url_for('views.home'))
+        
+
+    # show the form, it wasn't submitted
+    return render_template('edit_content.html',content_selected = user_content_data_sel)
+
+
+@views.route('/cancel_changes')
+def cancel_changes():
+    print(f'\ncancel_changes\n')
+    return redirect(url_for('views.home'))
+
+# TODO: Add delete content functionality
+@views.route('/delete_content')
+def delete_content():
+    print(f'\ndelete_content\n')
     return redirect(url_for('views.home'))
 
 @views.route('/delete_completed', methods=['POST'])
@@ -50,8 +132,6 @@ def complete_content(oid):
     content_item = mypct_cln.find_one({'_id': ObjectId(oid)})
     content_item['new'] = True
 
-
-
     current_content_title = 'This Is A Title'
     edited_content_title = 'Title Updated via Flask-completed'
 
@@ -60,3 +140,4 @@ def complete_content(oid):
     mypct_cln.update_one(filter=query,update=change)
     #mypct_cln.update_one(content_item)
     return redirect(url_for('views.home'))
+
