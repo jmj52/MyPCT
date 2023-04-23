4612683ac95231417dd980a588621c951ed34ddb
Joshua Jones <jmj52@iu.edu>
2023-04-22 23:15:54 -0400

    Fixing Add function, tweaking edit function

--- website/views.py
+++ website/views.py
@@ -30,19 +30,46 @@ def add_content():
     print(f'\nadd_content:{request.method}\n')
     mypct_cln = mongo.cx.mypct.tracker
 
+    user_email = session.get('User_Email')
     content_item = request.form.get('add-content')
     
     print(f'\nadd-content:{content_item}')
+    empty_dict = {'Title:':' ','Release_Date':' ','Type':' ','Rating':' ','Genre':' ','Notes':' ','Links':' '}
 
     # POST runs when user selects Save Changes
     if request.method == 'POST':
         content_item = request.form.getlist('add-content')
         print('content-passed:',content_item)
+ 
+        # Assuming Title is given 
+        #temp_title =  'temp_title'
+        #hange = {'$push':{'Content':{'Title': temp_title}}}
 
-        #mypct_cln.insert_one({'text': content_item})
+        # Update Database           
+        query = {'User_Email':user_email}
+        change = {'$push':{'Content':
+                           {'Title':            content_item[0],
+                            'Release_Date':     content_item[1],
+                            'Type':             content_item[2],
+                            'Rating':           content_item[3],
+                            'Genre':            content_item[4],
+                            'Notes':            content_item[5],
+                            'Links':            content_item[6],
+                           }                     
+                           }}
+        # change = {'$push':
+        #             {'Content.$.Title':          content_item[0],
+        #             'Content.$.Release_Date':    content_item[1],       
+        #             'Content.$.Type':            content_item[2],
+        #             'Content.$.Rating':          content_item[3],
+        #             'Content.$.Genre':           content_item[4],
+        #             'Content.$.Notes':           content_item[5],      
+        #             'Content.$.Links':           content_item[6],       
+        #             }}
+        mypct_cln.update_one(filter=query,update=change)
+        return redirect(url_for('views.home'))
 
-    empty_dict = {'Title:':' ','Release_Date':' ','Type':' ','Rating':' ','Genre':' ','Notes':' ','Links':' '}
-    return render_template('edit_content.html',content_selected = empty_dict, op = 'Add')
+    return render_template('add_content.html',content_selected = empty_dict)
 
 
 @views.route('/sel_content/<content_index>')
@@ -54,16 +81,17 @@ def sel_content(content_index):
 @views.route('/edit_content', methods=['GET', 'POST'])
 def edit_content():
     print(f'\nedit_content:{request.method}\n')
+
+    supported_fields = ['Title','Release_Date','Type','Rating','Genre','Notes','Links']
+    num_supported_fields=len(supported_fields)
     mypct_cln = mongo.cx.mypct.tracker
     
     # Get current content selected
     content_index = session.get('user_selection')
     selection_idx = int(content_index)-1
-    print('selection_idx:',selection_idx)
     
     # Get current User_Email selected
     user_email = session.get('User_Email')
-    print(user_email)
     query = {'User_Email':user_email}
     record = mypct_cln.find(query)
 
@@ -73,8 +101,6 @@ def edit_content():
 
     # Use index of selected content to show content
     user_content_data_sel = user_content_data[selection_idx]
-    #print(f'\nuser_data:\n{user_content_data_sel}')
-
     current_content_title = user_content_data_sel['Title']
 
     #Remove Artwork from results
@@ -85,6 +111,7 @@ def edit_content():
     if request.method == 'POST':
         content_item = request.form.getlist('edit-content')
         print('content-passed:',content_item)
+        print('user_content_data_sel',user_content_data_sel)
 
         # Assume Content Is Not Valid until we can validate the user input
         # Override to true for now, no validation criteria setup
@@ -98,20 +125,44 @@ def edit_content():
 
         # Content Is Valid, update database and return to content page
         else:
-            
-            # Update Database
             fields = list(user_content_data_sel.keys())
-            
-            query = {'_id':ObjectId(current_user_id),'Content.Title':current_content_title}
+            print(len(fields))
+
+            # Check to see if saved changes are different than existing content
+            keys_to_update = {}
+
+            keys = list(user_content_data_sel.keys())
+            for i in range(0,len(user_content_data_sel)):
+                existing_value  = list(user_content_data_sel.values())[i]
+                new_value       = content_item[i]
+                if new_value != '' and existing_value != new_value:
+                    print('change detected for',keys[i])
+                    keys_to_update.update({keys[i]:new_value})
+                else:
+                    keys_to_update.update({keys[i]:existing_value})
+
+            #print('keys_to_update:', keys_to_update)
+
+            # Build empty frame and add only non-empty fields 
+            changes = ['']*num_supported_fields
+
+            for i,k in enumerate(supported_fields):
+                if keys_to_update.get(k):
+                    changes[i] = keys_to_update[k]
+
+            #print(changes)
+
+            # Update Database           
+            query = {'User_Email':user_email,'Content.Title':current_content_title}
             
             change = {'$set':
-                      {'Content.$.Title':           content_item[0],
-                       'Content.$.Release_Date':    content_item[1],       
-                       'Content.$.Type':            content_item[2],
-                       'Content.$.Rating':          content_item[3],
-                       'Content.$.Genre':           content_item[4],
-                       'Content.$.Notes':           content_item[5],      
-                       'Content.$.Links':           content_item[6],       
+                      {'Content.$.Title':           changes[0],
+                       'Content.$.Release_Date':    changes[1],       
+                       'Content.$.Type':            changes[2],
+                       'Content.$.Rating':          changes[3],
+                       'Content.$.Genre':           changes[4],
+                       'Content.$.Notes':           changes[5],      
+                       'Content.$.Links':           changes[6],       
                         }}
             
             mypct_cln.update_one(filter=query,update=change)
@@ -120,7 +171,7 @@ def edit_content():
         
 
     # show the form, it wasn't submitted
-    return render_template('edit_content.html',content_selected = user_content_data_sel, op = 'Edit')
+    return render_template('edit_content.html',content_selected = user_content_data_sel)
 
 @views.route('/sort_content')
 def sort_content():
